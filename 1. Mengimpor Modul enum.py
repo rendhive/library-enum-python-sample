a45398fea2c5ce99d5ec4230603e656495da6064
Rendhive code <rendhivecode@gmail.com>
2026-02-08 23:56:22 +0700

    Add files via upload

---
+++ 1. Mengimpor Modul enum.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+print("Modul enum berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menggunakan enumerasi.
+# Kondisi: Digunakan sebelum mendefinisikan enum yang diperlukan dalam kode.
