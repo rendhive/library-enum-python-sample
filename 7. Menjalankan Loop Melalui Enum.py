3f81a1e2bc3f6b93a15e7e45a081bab95f221676
rendhive <rendhivecode@gmail.com>
2026-02-19 04:57:42 +0700

    [DOCT] Sample Library Python: 7. Menjalankan Loop Melalui Enum.py

---
+++ 7. Menjalankan Loop Melalui Enum.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Day(Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+for day in Day:
+    print(day.name)
+# Fungsi: Menjalankan loop untuk mengiterasi seluruh anggota enum.
+# Kondisi: Ketika Anda ingin melakukan tindakan untuk setiap anggota enum.
