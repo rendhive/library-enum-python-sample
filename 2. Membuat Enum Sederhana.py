922596b0c376f4ef8ab3ef93297fac35fe658db2
rendhive <rendhivecode@gmail.com>
2026-02-19 04:33:52 +0700

    [DOCT] Sample Library Python: 2. Membuat Enum Sederhana.py

---
+++ 2. Membuat Enum Sederhana.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+print(Color.RED)
+# Fungsi: Mendefinisikan enum untuk mengelompokkan konstanta warna.
+# Kondisi: Ketika Anda ingin merepresentasikan set nilai yang konstan dan terdefinisi.
